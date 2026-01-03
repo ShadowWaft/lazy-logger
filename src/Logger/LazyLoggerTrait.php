4722d55176e9b0a6edf5cab2d86b687fbaf66ed3
PascalMeinecke <pascal.meinecke1@web.de>
2026-01-03 09:33:14 +0100

    Logger trait adjustments
    - Added a magic getter method to make sure that a call with $this->logger can be done in any case. If no logger is initialized a NullLogger will be returned (Should not write any logs then).

--- src/Logger/LazyLoggerTrait.php
+++ src/Logger/LazyLoggerTrait.php
@@ -3,13 +3,23 @@
 namespace ShadowWaft\LazyLogger\Logger;
 
 use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 trait LazyLoggerTrait
 {
-    protected LoggerInterface $logger;
+    protected ?LoggerInterface $logger = null;
 
     public function setLazyLogger(LoggerInterface $logger): void
     {
         $this->logger = $logger;
+    }
+
+    public function __get($name)
+    {
+        if ($name === 'logger') {
+            return $this->logger ?? new NullLogger();
+        }
+
+        return null;
     }
 }
